530394c9f1969d956200f2dac0b3051b6c9f22aa
Mouaad <93816869+sMouaad@users.noreply.github.com>
2023-09-10 11:46:16 +0100

    Almost finished.

--- js/js.js
+++ js/js.js
@@ -1,14 +1,18 @@
-const btns = document.querySelectorAll(".choice.player");
 const gameResult = document.querySelector(".result");
-const round = [0,0];
-const rounds = document.querySelectorAll(".player")
 const audio = document.querySelectorAll("audio")
 const human = document.getElementById("human")
 const robot = document.getElementById("robot");
+
+let humanButtons = document.querySelectorAll(".choice.player");
+let robotButtons = document.querySelectorAll(".choice.robot");
+let robotHearts = document.querySelectorAll(".player2 .heart");
+let humanHearts = document.querySelectorAll(".player1 .heart");
+let heartIndex = [0,4]; // [0] : Robot [1] : Human;
+
 function smooth(e){
     e.target.classList.toggle("playerchoice");
     e.target.previousElementSibling.classList.toggle("flexy");
-    btns.forEach((btn)=>{
+    humanButtons.forEach((btn)=>{
         btn.removeEventListener("click",smooth);
     })
     let computerChoice = getComputerChoice();
@@ -18,7 +22,7 @@ function smooth(e){
     game(e.target.classList[0],computerChoice);
     //game(e.target.textContent);
 }
-btns.forEach((btn)=>{
+humanButtons.forEach((btn)=>{
     btn.addEventListener("click",smooth);
 })
 
@@ -51,42 +55,112 @@ function rockPaperScissors(playerchoice, computerChoice){
     }
 
 }
+function robotHurt(duration){
+    setTimeout(function (){
+        audio[1].play();
+        //takes 0.15 seconds to finish.
+        robot.classList.add("hurt");
+        decreaseHeart(1);
+        setTimeout(function(){
+            robot.classList.remove("hurt");
+        },audio[1].duration*100)
+    },duration);
 
+}
+function humanHurt(duration){
+    setTimeout(function (){
+        audio[0].play();
+        //Animation & filter when getting hurt
+        human.classList.add("hurt");
+        decreaseHeart(0);
+        setTimeout(function(){
+            human.classList.remove("hurt");
+        },audio[0].duration*100)
+    },duration);
+}
+
+function humanAttack(){
+    //Animation to attack : takes 2s to finish.
+    human.classList.add("attackright");
+    setTimeout(() => {
+        human.classList.remove("attackright");
+    },2000);
+    robotHurt(1600);
+}
+
+function robotAttack(){
+    //Animation to attack
+    robot.classList.add("attackleft");
+    setTimeout(() => {
+        robot.classList.remove("attackleft");
+    }, 2000);
+    humanHurt(1600);
+
+}
+
+function decreaseHeart(humanOrRobot){
+    if(!humanOrRobot){
+        humanHearts[heartIndex[1]].src = "./imgs/empty-heart.png";
+        humanHearts[heartIndex[1]].classList.add("flash");
+        //delay to remove the flash
+        setTimeout(()=>{
+            humanHearts[heartIndex[1]--].classList.remove("flash");
+        },100)
+    }
+    else{
+        robotHearts[heartIndex[0]].src = "./imgs/empty-heart.png";
+        robotHearts[heartIndex[0]].classList.add("flash");
+        //delay to remove the flash
+        setTimeout(()=>{
+            robotHearts[heartIndex[0]++].classList.remove("flash");
+        },100)
+    }
+}
+
+function initializeHearts(){
+    humanHearts.forEach((heart)=>{
+        heart.src= "./imgs/heart.png"
+    })
+    robotHearts.forEach((heart)=>{
+        heart.src= "./imgs/hardcore-heart.png"
+    })
+}
+function nextRound(){
+    robotButtons.forEach((btn)=>{
+        btn.previousElementSibling.classList.add("flexy");
+        btn.classList.remove("robotchoice");
+    })
+    humanButtons.forEach((btn)=>{
+        btn.previousElementSibling.classList.add("flexy");
+        btn.classList.remove("playerchoice");
+        btn.addEventListener("click",smooth);
+    })
+}
 function game(playerchoice,computerChoice){
     //one of them won, we stop the game.
-    if(round[0] === 5 || round[1] === 5){
+    console.log(`${heartIndex[0]} and ${heartIndex[1]+1}`);
+    if(heartIndex[0] === 5 || heartIndex[1]+1 === 0){
+        console.log("it stopped now :D")
         return
     }
     else{
         let result = rockPaperScissors(playerchoice,computerChoice);
         if(result.includes("win")){
-            rounds[0].textContent = ++round[0];
-            human.classList.remove("attackright");
-            human.classList.add("attackright");
-            setTimeout(function (){
-                audio[1].play();
-                robot.classList.add("hurt");
-                setTimeout(function(){
-                    robot.classList.remove("hurt");
-                },audio[1].duration*100)
-            },1600);
+            //this function takes around 2.15 seconds to finish.
+            humanAttack();
 
         }else if(result.includes("lose")){
-            rounds[1].textContent = ++round[1];
-            robot.classList.remove("attackleft");
-            robot.classList.add("attackleft");
-            setTimeout(function (){
-                audio[0].play();
-                human.classList.add("hurt");
-                setTimeout(function(){
-                    human.classList.remove("hurt");
-                },audio[0].duration*100)
-            },1600);
-            console.log("hi")
-            
+            //this function takes around 2.15 seconds to finish.
+            robotAttack();
         }
-        if(round[0] === 5 || round[1] === 5){
-            if(round[0]>round[1]) {
+        else{
+            console.log("tie");
+        }
+        setTimeout(nextRound,2400);
+        console.log(`second : ${heartIndex[0]} and ${heartIndex[1]+1}`);
+        if(heartIndex[0] === 5 || heartIndex[1]+1 === 0){
+            console.log("how are you now?")
+            if(heartIndex[1] === 0) {
                 gameResult.textContent="You won!"
             }
             else{
